7b910b5d22e8b97992c90e20457b83d8c6066586
Arman170616 <armanice09@gmail.com>
2023-09-07 14:31:14 +0600

    company end point

--- assets/urls.py
+++ assets/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
+
 
 urlpatterns = [
+    path('companies/',  CompanyList.as_view(), name='company-list'),
+    path('companies/<int:pk>/', CompanyDetail.as_view(), name='company-detail'),
+    
 
 ]
